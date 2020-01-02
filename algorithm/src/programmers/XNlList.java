161a7056f366a7f1e8431013452cfcbd59331f68
gganzing <gganzing@naver.com>
2020-01-02 13:53:17 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ algorithm/src/programmers/XNlList.java
@@ -0,0 +1,38 @@
+package programmers;
+
+import java.util.Arrays;
+
+//	함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 
+//	다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+//	x는 -10000000 이상, 10000000 이하인 정수입니다.
+//	n은 1000 이하인 자연수입니다.
+//	
+//	x	n	answer
+//	2	5	[2,4,6,8,10]
+//	4	3	[4,8,12]
+//	-4	2	[-4, -8]
+
+public class XNlList {
+
+	public static long[] solution(int x, int n) {
+		
+		long[] answer = new long[n];
+		
+		long r = 0;
+		for(int i=0; i<n; i++) {
+			r += x;
+			answer[i] = r;
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		
+		int x = 2;
+		int n = 5;
+		System.out.println(Arrays.toString(solution(x, n)));
+		
+	}
+	
+}
